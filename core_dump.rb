b2e93905d8edfe960c4e8bac9b0f3d1997186e85
Anıl Dalkılıç <57034057+devopsanilda@users.noreply.github.com>
2019-10-26 17:57:20 +0300

    Create core_dump.rb

---
+++ core_dump.rb
@@ -0,0 +1,23 @@
+# Cookbook Name:: hardening
+# Recipe:: core_dumps
+#
+# Begin xccdf_org.cisecurity.benchmarks_rule_1.6.1_Restrict_Core_Dumps
+
+case node['platform_family']
+when 'rhel'
+
+  # Ensure package is installed
+  package 'Install pam' do
+    package_name 'pam'
+    action :install
+  end
+
+  replace_or_add 'Restrict Core Dumps' do
+    path '/etc/security/limits.conf'
+    pattern '^\A\*\shard\score\s0'
+    line '* hard core 0'
+  end
+
+end
+
+# End xccdf_org.cisecurity.benchmarks_rule_1.6.1_Restrict_Core_Dumps
