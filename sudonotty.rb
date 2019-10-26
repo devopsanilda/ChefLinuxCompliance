375bc47206b1384c41278be09073ea659e3a8b94
Anıl Dalkılıç <57034057+devopsanilda@users.noreply.github.com>
2019-10-26 17:51:51 +0300

    Create sudonotty.rb

---
+++ sudonotty.rb
@@ -0,0 +1,20 @@
+# Recipe:: enable_sudo_no_tty
+#
+# Ensure sudo is installed
+
+package 'Install sudo' do
+  package_name 'sudo'
+  action :install
+end
+
+file '/etc/sudoers' do
+  mode 0440
+  owner 'root'
+  group 'root'
+  action :create
+end
+
+delete_lines 'remove hash-comments from /some/file' do
+  path '/etc/sudoers'
+  pattern '^.*requiretty'
+end
